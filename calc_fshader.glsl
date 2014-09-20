87f2ea934e0a2e4780ed7976f77e529f5939bfb9
Zhiming Guo <mjzshd@gmail.com>
2014-09-20 11:57:53 +0800

    Tweak

--- calc_fshader.glsl
+++ calc_fshader.glsl
@@ -22,9 +22,9 @@ void main() {
     if (count == 3 || (g == 1.0 && count == 2) ||
         (abs(xy.x - revive.x) < w * 5.0 && 
          abs(xy.y - revive.y) < h * 5.0))
-        gl_FragColor = vec4(0, 1, 0, 1);
+        gl_FragColor = vec4(0.1, 1, 0.2, 1);
     else if (g > 0.0)
-        gl_FragColor = vec4(0, g - 0.05, 0, 1);
+        gl_FragColor = vec4(0.1, g - 0.05, 0.2, 1);
     else 
         gl_FragColor = vec4(0.1, 0.0, 0.2, 1);
     //gl_FragColor = vec4(0, count == 3 || (g == 1.0 && count == 2), 0, 1);
